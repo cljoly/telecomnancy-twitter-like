d51e7b9a73c84ed8fa98e0ef005b3f4876100de6
JOLY Clément <clement.joly@telecomnancy.eu>
2019-04-28 15:01:14 +0200

    Mises en commun des constantes de la spec

--- src/server/const.h
+++ src/server/const.h
@@ -2,19 +2,11 @@
 #define CONST_H_KIGNQHQ7
 
 #define MAXVALUEINT 2147483647
+#include "spec_const.h"
 
 #define BUFSIZE 1500
 #define FDSET_SIZE_CLIENT 100
-#define DEFAULT_PORT 1234
 
 #define DB_NAME "db.sqlite3"
-
-// Codes d’erreur de la spec
-#define SPEC_ERR_FORMAT  10
-#define SPEC_ERR_INTERNAL_SRV  11
-#define SPEC_ERR_INTERNAL_CLIENT  12
-#define SPEC_ERR_NOT_IMPLEMENTED  13
-#define SPEC_ERR_DUPLICATE_USERNAME  1
-// TODO Continuer avec la spec
 
 #endif /* end of include guard: CONST_H_KIGNQHQ7 */
---
+++ src/server/spec_const.h
@@ -0,0 +1,14 @@
+#ifndef SPEC_CONST_H_DSA5DIFO
+#define SPEC_CONST_H_DSA5DIFO
+
+// Codes d’erreur de la spec
+#define SPEC_ERR_FORMAT  10
+#define SPEC_ERR_INTERNAL_SRV  11
+#define SPEC_ERR_INTERNAL_CLIENT  12
+#define SPEC_ERR_NOT_IMPLEMENTED  13
+#define SPEC_ERR_DUPLICATE_USERNAME  1
+// TODO Continuer avec la spec
+
+#define DEFAULT_PORT 1234
+
+#endif /* end of include guard: SPEC_CONST_H_DSA5DIFO */
